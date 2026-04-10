25aff78dc0f245097f9234582539de5a1b847e24
Tio Pramana <tiopramanaa@gmail.com>
2026-04-10 13:50:39 +0800

    New list consept

--- data_structure/list.py
+++ data_structure/list.py
@@ -62,3 +62,58 @@ print( "apel" in buah)
 print(buah.append("lolok"))
 
 print(buah)
+
+# Unpacking List Packet 
+
+# Contoh 1
+fruit = ['mangga', 'jeruk', 'apel', 'nanas']
+first_fruit, second_fruit, *rest = fruit
+
+print(first_fruit)
+print(second_fruit)
+print(rest)
+
+# Contoh 2
+
+list = [1,2,3,4,5,6,7,8,9,10]
+first, second, *rest, end = list
+
+print(first)
+print(second)
+print(rest)
+print(end)
+
+# Contoh 3
+
+countries = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','Estonia']
+gr, fr, be, sw, *sisa, ic, es, = countries
+
+print(gr)
+print(fr)
+print(be)
+print(sw)
+print(sisa)
+print(ic)
+print(es)
+
+# Slicing item from list 
+president = ['Jokowi', 'Prabowow', 'Anies', 'Ganjar']
+print(president[0:4]) # Memprint array atau list dari index 0 -> 4 yaitu semuanya
+
+print(president[0:]) # Memprint arry dari data 0 seterus nya hingga akhir
+
+print(president[1:2]) # Memprint array yang melewati index 1 dan 3 4
+
+print(president[::2]) # Step locatan data 2 kali dari 1 -> 2 -> 3 -> 4
+
+# Menggunakan negatif 
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits[-4:]) # Memprint array atau list dari index 0 -> 4 yaitu semuanya
+
+print(fruits[-3:-1]) # Tidak memprint index pertama dan terakhir
+
+print(fruits[-3:]) # Print yang dimulai dari index melewati -4 atau yang pertama
+
+print(fruits[::-1]) # Membalik list nya dari -4 -3 -2 -1 menjadi -1 -2 -3 -4 begitu
+
