e26e2ae0449c312ecf808b375e08f2f00b3dbfd7
Tio Pramana <tiopramanaa@gmail.com>
2026-04-17 23:30:00 +0800

    new: material for module

---
+++ module/module.py
@@ -0,0 +1,11 @@
+# Module adalah code yang sudah include atau tersedia didalam program
+# dengan menggunakan command import untuk melengkapi module sesuai dengan yang dibutuhkan
+# berguna untuk mengurasi pengerjaan program yang rumit menjadi lebih mudah dengna module
+
+# print(help("modules"))
+
+import math
+
+a = math.pi
+
+print(a)
