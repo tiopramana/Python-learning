cfa3f3f7228e83632758a10717fdc215bdb524e3
Tio Pramana <tiopramanaa@gmail.com>
2026-09-04 23:58:46 +0800

    just bored making fizzbuzz

---
+++ Percobaan/FizzBuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz(item):
+
+    for i in item:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+fizzbuzz(range(1,11))
